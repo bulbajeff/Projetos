a7cb8e97aa189c469e5278708ea73b0953b4cbd1
Jefferson Sambuichi <samuraijeff@outlook.com>
2023-03-16 15:58:29 -0300

    mercadinho

---
+++ Mercadinho/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Mercadinho;
+
+List<Produto> merca1 = new List<Produto>();
+List<Produto> merca2 = new List<Produto>();
+List<Produto> merca3 = new List<Produto>();
+
+bool continua = true;
+
+while (continua)
+{
+    Console.WriteLine("Digite o nome do produto a ser cadastrado: ");
+    string nomeProduto = Console.ReadLine();
+
+    Console.WriteLine("Agora digite o valor do produto para o mercado 1: ");
+    double precoProduto = int.Parse(Console.ReadLine());
+
+
+    Console.WriteLine("Pressione S para continuar cadastrando ou N para encerrar");
+    string resposta = Console.ReadLine();
+
+
+
+    if(resposta == "N")
+    {
+        continua= false;
+    }
+}
